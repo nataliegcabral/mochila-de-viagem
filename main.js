d00a9cbd4d588d9c9e23752e726d2945866c4ad8
Natalie Cabral <nataliegc23@gmail.com>
2022-12-05 12:49:17 -0300

    update

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
 const form = document.querySelector('#novoItem')
+const lista = document.querySelector('#lista')
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+})
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault()
     //previnir de enviar para a própria página, que tá como padrão
 
-    
-    
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
-    
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+        const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    criaElemento(itemAtual)
+
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = "";
+    quantidade.value = "";
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
 
     const novoItem = document.createElement('li')
     novoItem.classList.add('item')
 
     const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
-    
+    numeroItem.innerHTML = item.quantidade
     novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
 
-    const lista = document.querySelector('#lista')
-
+    novoItem.innerHTML += item.nome
+    
     lista.appendChild(novoItem)
+    
 
 }
 
