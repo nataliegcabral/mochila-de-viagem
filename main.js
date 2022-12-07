46dd506305041997dd4e60f86be1bc1a9aa2cf6b
Natalie Cabral <nataliegc23@gmail.com>
2022-12-06 23:27:05 -0300

    updates

--- main.js
+++ main.js
@@ -13,15 +13,31 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
         const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+
+    } else {
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
+        //lê-se como: se não existe nada no array, o id é 0, se tiver algo no id, quero achar no úlitmo elemento, o id e adicionar 1 a ele
+        //operador ternário (?): se positivo (primeira condição) ? : se negativo (segunda posição) 
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
 
 
-    itens.push(itemAtual)
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -36,14 +52,39 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome
-    
+
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem)
     
+    
+}
 
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
 
 
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement('button')
+    elementoBotao.innerText = 'x'
 
+    elementoBotao.addEventListener('click', function() {
+        deletaElemento(this.parentNode, id)
+    })
+    //arrow function não permite que seja usado o 'this', que serve para especificar que aquele elemento foi clicado.
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+}
